8e1cfd826ef6b8527169f7ef645c973706a72787
moshanghki <moshanghki@163.com>
2019-07-03 11:37:41 +0800

    字符反转

---
+++ redeme.md
@@ -0,0 +1,10 @@
+## 相关地址
+***
+[leetcode中文网地址](https://leetcode-cn.com/problemset/all/)
+
+**这是加粗的字体**
+*this is*
+**this is**
+*这里是正常的字体*
+> 这里是引用的内容
+
---
+++ test/string/reverse-word.test.js
@@ -0,0 +1,5 @@
+import reverseWord from './../../code/string/reverse-word.js'
+
+test('reverse word', () => {
+  expect(reverseWord('Hello world!')).toBe('olleH !dlrow')
+})
